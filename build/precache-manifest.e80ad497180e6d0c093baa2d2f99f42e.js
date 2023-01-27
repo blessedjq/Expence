2b5613c30e5940cb8613ec045ae88aa8e8af9c3b
blessedjq <68956036+blessedjq@users.noreply.github.com>
2023-01-27 11:23:15 +0530

    Add files via upload

---
+++ build/precache-manifest.e80ad497180e6d0c093baa2d2f99f42e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "1d26bf90a79a03243e9e",
+    "url": "/expen/static/css/main.486c88cd.chunk.css"
+  },
+  {
+    "revision": "1d26bf90a79a03243e9e",
+    "url": "/expen/static/js/main.707847ea.chunk.js"
+  },
+  {
+    "revision": "2bca0130adab4a6d239f",
+    "url": "/expen/static/js/runtime~main.197a30ea.js"
+  },
+  {
+    "revision": "1c1fc3d9f03484cac373",
+    "url": "/expen/static/js/2.42e78c26.chunk.js"
+  },
+  {
+    "revision": "22ea7704a711b34e9aa4939da910f9d0",
+    "url": "/expen/index.html"
+  }
+];
